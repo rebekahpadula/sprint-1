520b6c24d68a8280d2a010fcf180b274f71b1fc6
Rebekah <rebekah.adair@yahoo.com>
2016-03-07 21:04:20 -0500

    getting closer

--- src/js/main.js
+++ src/js/main.js
@@ -13,49 +13,81 @@
     ];
 
     var songsList = document.querySelector('#songs-list');
-    var addSong = document.querySelector('.create-form-add-song');
     var addButton = document.querySelector('#add-button');
     var numSongs = document.querySelector('.num-songs');
     var cancel = document.querySelector('.create-form-cancel');
     var formAdd = document.querySelector('.create-form-add');
     var createForm = document.querySelector('#create-form');
-    var hidden = document.querySelector('.hidden');
+    var titleInput = document.querySelector('#title-input');
+    var artistInput = document.querySelector('#artist-input');
+    var genreInput = document.querySelector('#genre-input');
 
     function renderSongs (song) {
         songsList.innerHTML = '';
     }
 
+    // creating new song and making in into html
+    function getsongEl (newSong) {
+        var el = document.createElement('div');
+        el.classList.add('newSong');
+        el.innerHTML =
+        '<div class="newSong">' +
+            '<h3>' + newSong.title + '</h3>' +
+            '<h4>' + newSong.artist + '</h4>' +
+            '<h4>' + newSong.genre + '</h4>' +
+        '</div>';
+        return el;
+    };
+
+
     // number of songs
     function songsLength (data) {
-        numSongs.innerHTML = data.length;
+        newSong.innerHTML = data.length;
     }
 
-    // adding song to data array
-    function getsongEl (song) {
-        var el = document.createElement('div');
-        el.classList.Add('song');
-        el.innerHTML =
-        '<div class="song-content">' +
-            '<h1>' + song.title + '</h1>' +
-            '<h2>By ' + song.artist + '</h2>' +
-            '<h2>' + song.genre + '</h2>' +
-        '</div>' +
-        '</div>';
-    }
-
+    // pushing song to data array
     function pushSong (song) {
         data.push(song);
-        return song;
+        render(song);
+    }
+
+    // appending child to display. el comes from getsongEl function
+    // el becomes a real html element.
+    function render (song) {
+       var el = getsongEl(song);
+       songsList.appendChild(el);
+    }
+
+    // looping through data array with forEach
+    function initialize () {
+        data.forEach(render);
     }
 
     // hiding/showing add form
     addButton.addEventListener('click', function () {
-        if (createForm.classList.contains('createForm')) {
-            createForm.classList.remove('createForm');
-            createForm.classList.add('hidden');
-        } else {
-            createForm.classList.remove('hidden');
-            createForm.classList.add('createForm');
-        }
+        createForm.classList.add('active');
+        addButton.classList.add('hide-button');
     });
-});
+
+    cancel.addEventListener('click', function () {
+        createForm.classList.remove('active');
+        addButton.classList.remove('hide-button');
+    });
+
+    // using user inputs to add to object that gets pushed into data array
+    formAdd.addEventListener('click', function () {
+        var inputs = {
+            title: titleInput.value,
+            artist: artistInput.value,
+            genre: genreInput.value
+        };
+        createForm.classList.remove('active');
+        titleInput.value = '';
+        artistInput.value = '';
+        genreInput.value = '';
+        pushSong(inputs);
+        addButton.classList.remove('hide-button');
+    });
+
+    initialize();
+})();
